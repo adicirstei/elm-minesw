2eec6e31051ee07b38a0d5cd2d75063aa74bc8c2
Adrian Cirstei <adicirstei@gmail.com>
2016-11-13 23:12:47 +0200

    svg

--- Game/View.elm
+++ Game/View.elm
@@ -1,108 +1,171 @@
 module Game.View exposing (root)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html exposing (Html)
+
+
+--import Html.Attributes exposing (..)
+--import Html.Events exposing (onClick)
+
 import String exposing (padLeft)
 import Dict exposing (Dict)
 import Game.Types exposing (..)
 import Html.App
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Svg.Events exposing (onClick)
+
+
+--import Svg.Attributes exposing (..)
 
 
 root : Model -> Html Msg
 root model =
-    case model of
-        Playing gm ->
-            drawBoard gm
-                |> Html.App.map PlayMsg
+    svg [ width "640", height "480" ]
+        (case model of
+            Playing gm ->
+                [ drawBoard gm
+                    |> Html.App.map PlayMsg
+                ]
 
-        GameWon gm ->
-            div []
+            GameWon gm ->
                 [ drawBoard gm
                     |> Html.App.map (\_ -> GoMsg GoRestart)
-                , div
-                    [ style
-                        [ ( "position", "absolute" )
-                        , ( "top", "100px" )
-                        , ( "left", "50px" )
-                        , ( "font-size", "64px" )
-                        , ( "padding", "24px" )
-                        , ( "background", "yellow" )
-                        ]
-                    ]
+                , rect []
                     [ text "Victory!" ]
                 ]
 
-        GameLost gm ->
-            div []
+            GameLost gm ->
                 [ drawBoard gm |> Html.App.map (\_ -> GoMsg GoRestart)
-                , div
-                    [ style
-                        [ ( "position", "absolute" )
-                        , ( "top", "100px" )
-                        , ( "left", "50px" )
-                        , ( "font-size", "64px" )
-                        , ( "padding", "24px" )
-                        , ( "background", "yellow" )
-                        ]
-                    ]
+                , rect
+                    []
                     [ text "You lost!" ]
                 ]
+        )
+
+
+
+--
+-- root9 : Model -> Html Msg
+-- root9 model =
+--     case model of
+--         Playing gm ->
+--             drawBoard gm
+--                 |> Html.App.map PlayMsg
+--
+--         GameWon gm ->
+--             div []
+--                 [ drawBoard gm
+--                     |> Html.App.map (\_ -> GoMsg GoRestart)
+--                 , div
+--                     [ style
+--                         [ ( "position", "absolute" )
+--                         , ( "top", "100px" )
+--                         , ( "left", "50px" )
+--                         , ( "font-size", "64px" )
+--                         , ( "padding", "24px" )
+--                         , ( "background", "yellow" )
+--                         ]
+--                     ]
+--                     [ text "Victory!" ]
+--                 ]
+--
+--         GameLost gm ->
+--             div []
+--                 [ drawBoard gm |> Html.App.map (\_ -> GoMsg GoRestart)
+--                 , div
+--                     [ style
+--                         [ ( "position", "absolute" )
+--                         , ( "top", "100px" )
+--                         , ( "left", "50px" )
+--                         , ( "font-size", "64px" )
+--                         , ( "padding", "24px" )
+--                         , ( "background", "yellow" )
+--                         ]
+--                     ]
+--                     [ text "You lost!" ]
+--                 ]
 
 
 drawBoard : GameData -> Html PlayMsg
 drawBoard gm =
-    div
-        [ style
-            [ ( "font-family", "monospace" )
-            , ( "font-size", "24px" )
-            , ( "width", "-webkit-min-content" )
-            , ( "width", "-moz-min-content" )
-            , ( "width", "min-content" )
-            ]
-        ]
-        [ div
-            [ class "header"
-            , style [ ( "width", "100%" ), ( "background", "#222" ) ]
-            ]
-            [ div
-                [ style
-                    [ ( "float", "left" )
-                    , ( "color", "red" )
-                    , ( "width", "33.3333%" )
-                    ]
-                ]
+    rect
+        [ x "0", y "0", width "640", height "480" ]
+        [ rect
+            [ fill "black", x "0", y "0", width "640", height "48" ]
+            [ rect
+                [ x "0", y "0", width "64", height "48", stroke "red" ]
                 [ text (gm.minesCount |> format) ]
-            , div
-                [ style
-                    [ ( "color", "black" )
-                    , ( "background", "yellow" )
-                    , ( "width", "48px" )
-                    , ( "margin", "0 auto" )
-                    ]
-                , onClick Restart
+            , rect
+                [ onClick Restart
                 ]
                 [ text ":-)" ]
-            , div
-                [ style
-                    [ ( "float", "right" )
-                    , ( "color", "red" )
-                    , ( "width", "33.3333%" )
-                    , ( "text-align", "right" )
-                    ]
-                ]
+            , rect
+                [ x "500", y "0", width "64", height "48", stroke "red" ]
                 [ text (format gm.time) ]
             ]
-        , div
+        , rect
             [ class "grid"
-            , style
-                [ ( "background-color", "white" )
-                , ( "font-size", "16px" )
-                , ( "clear", "both" )
-                ]
             ]
             (renderGrid gm)
         ]
+
+
+
+--
+--
+-- drawBoard : GameData -> Html PlayMsg
+-- drawBoard gm =
+--     div
+--         [ style
+--             [ ( "font-family", "monospace" )
+--             , ( "font-size", "24px" )
+--             , ( "width", "-webkit-min-content" )
+--             , ( "width", "-moz-min-content" )
+--             , ( "width", "min-content" )
+--             ]
+--         ]
+--         [ div
+--             [ class "header"
+--             , style [ ( "width", "100%" ), ( "background", "#222" ) ]
+--             ]
+--             [ div
+--                 [ style
+--                     [ ( "float", "left" )
+--                     , ( "color", "red" )
+--                     , ( "width", "33.3333%" )
+--                     ]
+--                 ]
+--                 [ text (gm.minesCount |> format) ]
+--             , div
+--                 [ style
+--                     [ ( "color", "black" )
+--                     , ( "background", "yellow" )
+--                     , ( "width", "48px" )
+--                     , ( "margin", "0 auto" )
+--                     ]
+--                 , onClick Restart
+--                 ]
+--                 [ text ":-)" ]
+--             , div
+--                 [ style
+--                     [ ( "float", "right" )
+--                     , ( "color", "red" )
+--                     , ( "width", "33.3333%" )
+--                     , ( "text-align", "right" )
+--                     ]
+--                 ]
+--                 [ text (format gm.time) ]
+--             ]
+--         , div
+--             [ class "grid"
+--             , style
+--                 [ ( "background-color", "white" )
+--                 , ( "font-size", "16px" )
+--                 , ( "clear", "both" )
+--                 ]
+--             ]
+--             (renderGrid gm)
+--         ]
 
 
 renderGrid model =
@@ -110,7 +173,7 @@ renderGrid model =
 
 
 renderRow model row =
-    div
+    rect
         [ class "columns is-gapless is-small"
           -- style
           --     [ ( "height", "28px" )
@@ -124,35 +187,16 @@ renderRow model row =
 renderCell model row col =
     case Dict.get ( row, col ) model.grid |> Maybe.withDefault Hidden of
         Hidden ->
-            div
-                [ class "column is-small"
-                , style
-                    [ ( "width", "24px" )
-                    , ( "height", "24px" )
-                    , ( "margin", "1px" )
-                    , ( "display", "inline-block" )
-                    , ( "background-color", "gray" )
-                    , ( "border", "1px solid #333" )
-                    ]
-                , onClick (CellClicked ( row, col ))
+            rect
+                [ onClick (CellClicked ( row, col ))
                 ]
                 []
 
         Visible mines ->
-            div
+            rect
                 [ class "column"
-                , style
-                    [ ( "width", "24px" )
-                    , ( "height", "24px" )
-                    , ( "margin", "1px" )
-                    , ( "display", "inline-block" )
-                    , ( "background-color", "lightgray" )
-                    , ( "vertical-align", "top" )
-                    , ( "border", "1px solid lightgray" )
-                    , ( "color", color mines )
-                    , ( "font-weight", "bold" )
-                    , ( "text-align", "center" )
-                    ]
+                , width "24"
+                , height "24"
                 ]
                 [ if mines == 0 then
                     text ""
@@ -161,35 +205,105 @@ renderCell model row col =
                 ]
 
         Mine ->
-            div
+            rect
                 [ class "column"
-                , style
-                    [ ( "width", "24px" )
-                    , ( "height", "24px" )
-                    , ( "margin", "1px" )
-                    , ( "display", "inline-block" )
-                    , ( "background-color", "red" )
-                    , ( "vertical-align", "top" )
-                    , ( "border", "1px solid darkred" )
-                    , ( "font-weight", "bold" )
-                    , ( "text-align", "center" )
-                    ]
+                , width "24"
+                , height "24"
                 ]
                 [ text "*"
                 ]
 
         _ ->
-            div
+            rect
                 [ class "column"
-                , style
-                    [ ( "width", "24px" )
-                    , ( "height", "24px" )
-                    , ( "margin", "1px" )
-                    , ( "display", "inline-block" )
-                    , ( "background-color", "gray" )
-                    ]
+                , width "24"
+                , height "24"
                 ]
                 []
+
+
+
+-- renderRow model row =
+--     div
+--         [ class "columns is-gapless is-small"
+--           -- style
+--           --     [ ( "height", "28px" )
+--           --     , ( "clear", "both" )
+--           --     , ( "display", "inline-flex" )
+--           --     ]
+--         ]
+--         ([0..model.cols - 1] |> List.map (renderCell model row))
+--
+--
+-- renderCell model row col =
+--     case Dict.get ( row, col ) model.grid |> Maybe.withDefault Hidden of
+--         Hidden ->
+--             div
+--                 [ class "column is-small"
+--                 , style
+--                     [ ( "width", "24px" )
+--                     , ( "height", "24px" )
+--                     , ( "margin", "1px" )
+--                     , ( "display", "inline-block" )
+--                     , ( "background-color", "gray" )
+--                     , ( "border", "1px solid #333" )
+--                     ]
+--                 , onClick (CellClicked ( row, col ))
+--                 ]
+--                 []
+--
+--         Visible mines ->
+--             div
+--                 [ class "column"
+--                 , style
+--                     [ ( "width", "24px" )
+--                     , ( "height", "24px" )
+--                     , ( "margin", "1px" )
+--                     , ( "display", "inline-block" )
+--                     , ( "background-color", "lightgray" )
+--                     , ( "vertical-align", "top" )
+--                     , ( "border", "1px solid lightgray" )
+--                     , ( "color", color mines )
+--                     , ( "font-weight", "bold" )
+--                     , ( "text-align", "center" )
+--                     ]
+--                 ]
+--                 [ if mines == 0 then
+--                     text ""
+--                   else
+--                     text (toString mines)
+--                 ]
+--
+--         Mine ->
+--             div
+--                 [ class "column"
+--                 , style
+--                     [ ( "width", "24px" )
+--                     , ( "height", "24px" )
+--                     , ( "margin", "1px" )
+--                     , ( "display", "inline-block" )
+--                     , ( "background-color", "red" )
+--                     , ( "vertical-align", "top" )
+--                     , ( "border", "1px solid darkred" )
+--                     , ( "font-weight", "bold" )
+--                     , ( "text-align", "center" )
+--                     ]
+--                 ]
+--                 [ text "*"
+--                 ]
+--
+--         _ ->
+--             div
+--                 [ class "column"
+--                 , style
+--                     [ ( "width", "24px" )
+--                     , ( "height", "24px" )
+--                     , ( "margin", "1px" )
+--                     , ( "display", "inline-block" )
+--                     , ( "background-color", "gray" )
+--                     ]
+--                 ]
+--                 []
 
 
 format =
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ try it here: https://adicirstei.github.io/elm-minesw/
 
 * started styling with [Bulma](http://bulma.io/documentation/grid/columns/)
 * or maybe [elm-mdl](https://debois.github.io/elm-mdl/#buttons)
+* or, why not, svg. I think it makes a lot more sense for a game.
